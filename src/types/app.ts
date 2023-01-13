641031bcc41cb3f8593e35ee686624483c64373f
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-01-13 13:58:23 -0800

    merge main

--- src/types/app.ts
+++ src/types/app.ts
@@ -12,16 +12,14 @@ export interface AppUpdateProps {
 }
 
 export interface App {
-  id?: string; // leaving those for now to not break non beta implementation, should be removed and replaced by api returned values below
-  name?: string;  // leaving those for now to not break non beta implementation, should be removed and replaced by api returned values below
-  icon?: string;  // leaving those for now to not break non beta implementation, should be removed and replaced by api returned values below
-
   appId: string;
   handcashAppId: string;
   appName: string;
   appImage: string;
   teamId: string;
   status: string;
+  description: string;
+  url: string;
   createdAt: number;
   updatedAt: number;
   slots: string[];

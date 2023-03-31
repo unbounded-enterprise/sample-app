e85ee95c2e661528b3ac5f439fcffb62c436fbdc
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-03-31 14:34:40 -0700

    NftDetailDisplay semantics

--- src/components/NftDetailDisplay.js
+++ src/components/NftDetailDisplay.js
@@ -2,55 +2,67 @@ import { Box, Button, Card, FormControl, Grid, InputLabel, Typography, Select, M
 import dynamic from 'next/dynamic'
 import React from 'react';
 
-
-
 const DisplayNFTWithNoSSR = dynamic(
-    () => import('src/components/DisplayNFT'),
-    { ssr: false }
-  )
+  () => import('src/components/DisplayNFT'),
+  { ssr: false }
+)
 
-export const NftPropertyDisplay = ({nft, properties, setProperties}) => {
-    var select = e =>{
-      if(nft.properties[e.target.value]){
-      setProperties(Object.entries(nft.properties[e.target.value]));}
-      else{
-        setProperties(null);
-      }
+const slotButtonStyle = { color: 'white', border: '1px solid white', fontSize: '4vw' };
+var propertyString = "No Properties Set";
+var expressionNames = [];
+var apps = [""];
+var select = (e) => {};
+
+export const NftPropertyDisplay = ({ nft, properties, setProperties }) => {
+  apps = [""];
+  select = (e) => {
+    if (nft.properties[e.target.value]) {
+      setProperties(Object.entries(nft.properties[e.target.value]));
     }
-    var apps = [""];
-    if(nft.properties){
-      apps = apps.concat(Object.keys(nft.properties));
-    } else {
-      apps = ["none found"];
+    else {
+      setProperties(null);
     }
-    if(!nft){
-      nft = {nftId:1};
-    }
-    return(
-      <Grid
+  }
+
+  if (nft.properties) {
+    apps = apps.concat(Object.keys(nft.properties));
+  } else {
+    apps = ["none found"];
+  }
+
+  if (!nft) {
+    nft = { nftId: 1 };
+  }
+
+  return (
+    <Grid
       item
       key={nft.nftId}
       xs={12}
-  >
-    <Typography variant="p2" sx={{ alignSelf:"end", fontWeight:'bold',fontSize:{ xs: '12px', sm: '14px', md: '16px', lg: '16px', xl: '18px'}}}>Properties &emsp;</Typography>
-          <FormControl sx={{width:"20%", right:0, p:1}}>
-            <InputLabel id="demo-simple-select-label">Select App</InputLabel>
-             <Select
-                defaultValue={""}
-                value={apps}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Select App"
-                onChange={select}
-              >
-              {apps.map((app) => (
-                  <MenuItem key={app} value={app}>{app}</MenuItem>  
-             ))}
-            </Select>
-        </FormControl>
-        
-          <Box sx={{width:650}}><TableContainer>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    >
+      <Typography variant="p2" sx={{ alignSelf:"end", fontWeight:'bold',fontSize:{ xs: '12px', sm: '14px', md: '16px', lg: '16px', xl: '18px'}}}>
+        Properties &emsp;
+      </Typography>
+      <FormControl sx={{ width:"20%", right:0, p:1 }}>
+        <InputLabel id="demo-simple-select-label">Select App</InputLabel>
+        <Select
+          defaultValue={""}
+          value={apps}
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Select App"
+          onChange={select}
+        >
+          { apps.map((app) => (
+            <MenuItem key={app} value={app}>
+              { app }
+            </MenuItem>  
+          )) }
+        </Select>
+      </FormControl>
+      <Box sx={{ width: 650 }}>
+        <TableContainer>
+          <Table aria-label="simple table" sx={{ minWidth: 650 }}>
             <TableHead>
               <TableRow>
                 <TableCell>Property</TableCell>
@@ -58,76 +70,80 @@ export const NftPropertyDisplay = ({nft, properties, setProperties}) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {properties && properties.map((property) => (
+              { properties && properties.map((property) => (
                 <TableRow
                   key={property[0]}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
-                    {property[0]}
+                    { property[0] }
                   </TableCell>
-                  <TableCell align="right">{JSON.stringify(property[1])}</TableCell>
+                  <TableCell align="right">
+                    { JSON.stringify(property[1]) }
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-          </Box>          
-        </Grid>)
+      </Box>          
+    </Grid>
+  )
+}
+
+export const NftDetailDisplay = ({ nft, setCurrentExpression, currentExpression }) => {
+  propertyString = "No Properties Set";
+  expressionNames = [];
+
+  if (nft.properties) {
+    propertyString = JSON.stringify(nft.properties, 2, null);
+  }
+  
+  if (nft.expressionValues) {
+    nft.expressionValues.forEach(element => {
+      if(!expressionNames.includes(element.expression.expressionName)){
+        expressionNames.push(element.expression.expressionName);
+      }
+    });
   }
 
-export const NftDetailDisplay = ({nft, setCurrentExpression, currentExpression}) => {
-    var slotButtonStyle = { color: 'white', border: '1px solid white', fontSize: '4vw'};
-    var propertyString = "No Properties Set";
-    if(nft.properties){
-        propertyString = JSON.stringify(nft.properties, 2, null);
-    }
-    var expressionNames = [];
-    if(nft.expressionValues){
-        nft.expressionValues.forEach(element => {
-            if(!expressionNames.includes(element.expression.expressionName)){
-              expressionNames.push(element.expression.expressionName);
-            }
-        });
-    }
-    return (
-        <Grid
-        item
-        key={nft.nftId}
-        xs={12}
-        onClick={()=>{
-          //setChosenNFT(nft);
-        }}
+  return (
+    <Grid
+      item
+      key={nft.nftId}
+      xs={12}
+      onClick={()=>{
+        //setChosenNFT(nft);
+      }}
     >
-        <Card
-          sx={{
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            p: 1,
-            m: 1,
-          }}
-          variant="outlined"
-        >
-              <DisplayNFTWithNoSSR 
-                assetlayerNFT={nft}
-                expression={currentExpression}
-                //defaultAnimation={'durodog_idle_1'} // this would need to be abstracted. maybe look for idle or just go alphabetical?
-                // defaultAnimation={defaultAnimation}
-                showAnimations={true}
-                // animationAlign={isMobileDevice?'top':'right'}
-                nftSizePercentage={65}
-                // onLoaded={onLoaded}
-            />
-        </Card>
-        {expressionNames.map((name) => (
-          <React.Fragment key={name}>
-            <Button onClick={()=>{
-                setCurrentExpression(name);
-            }
-            }>{name}</Button>          
-          </React.Fragment>
-             ))}
+      <Card
+        variant="outlined"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          p: 1,
+          m: 1,
+        }}
+      >
+        <DisplayNFTWithNoSSR 
+          assetlayerNFT={nft}
+          expression={currentExpression}
+          //defaultAnimation={'durodog_idle_1'} // this would need to be abstracted. maybe look for idle or just go alphabetical?
+          // defaultAnimation={defaultAnimation}
+          showAnimations={true}
+          // animationAlign={isMobileDevice?'top':'right'}
+          nftSizePercentage={65}
+          // onLoaded={onLoaded}
+        />
+      </Card>
+      { expressionNames.map((name) => (
+        <React.Fragment key={name}>
+          <Button onClick={()=>{ setCurrentExpression(name); }}>
+            { name }
+          </Button>          
+        </React.Fragment>
+      )) }
     </Grid>
-      )
-  }
+  )
+}

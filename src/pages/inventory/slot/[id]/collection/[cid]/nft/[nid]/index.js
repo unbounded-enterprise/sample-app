d5bac334bf86369dd80fc92335aab4356dcce1b9
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-04-04 22:22:25 -0700

    nid (inventory) syntax

--- src/pages/inventory/slot/[id]/collection/[cid]/nft/[nid]/index.js
+++ src/pages/inventory/slot/[id]/collection/[cid]/nft/[nid]/index.js
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react';
 import NextLink from 'next/link';
-import {useRouter} from 'next/router';
-import { Box, Breadcrumbs, Typography, Link, Grid } from '@mui/material';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { Box, Breadcrumbs, Grid, Link, Typography } from '@mui/material';
 import { NewLayout } from 'src/components/new-layout';
 import axios from 'axios';
-import React from 'react';
 import { NftDetailDisplay, NftPropertyDisplay } from 'src/components/inventory/NftDetailDisplay';
+
+const slotButtonStyle = { color: 'blue', border: '1px solid blue', fontSize: '1vw' };
+const emptyNode = <></>;
 
 const InventoryNftDetailPage = ()=>{
   const router = useRouter();
@@ -15,110 +17,118 @@ const InventoryNftDetailPage = ()=>{
   const [chosenCollection, setChosenCollection]  =  useState(null);
   const [chosenSlot, setChosenSlot] = useState(null);
   const [chosenNft, setChosenNft] = useState(null);
- const [properties, setProperties] = useState(null);
+  const [properties, setProperties] = useState(null);
   const [currentExpression, setCurrentExpression] = useState("Menu View");
-
 
   const [slotId, setSlotId] = useState(null);
   const [collectionId, setCollectionId] = useState(null);
   const [nftId, setNftId] = useState(null);
-
-  const slotButtonStyle = { color: 'blue', border: '1px solid blue', fontSize: '1vw'};
   
   useEffect(()=>{
-    if(router.isReady){
-        setSlotId(router.asPath.split("/")[3]);
-        setCollectionId(router.asPath.split("/")[5]);
-        setNftId(router.asPath.split("/")[7]);
+    if (router.isReady) {
+      setSlotId(router.asPath.split("/")[3]);
+      setCollectionId(router.asPath.split("/")[5]);
+      setNftId(router.asPath.split("/")[7]);
     }
   }, [router.isReady]);
 
-  useEffect(()=>{
-    if(slotId){
-    getSlot(slotId).then((slot)=>{
-        setChosenSlot(slot)})
-        .catch(e=>{console.log('setting error: ', e.message)});}
+  useEffect(() => {
+    if (slotId) {
+      getSlot(slotId)
+        .then((slot) => {
+          setChosenSlot(slot);
+        })
+        .catch((e) => {
+          console.log('setting error: ', e.message);
+        });
+    }
   }, [slotId]);
 
   useEffect(()=>{
-    if(collectionId){
-        getCollection(collectionId, sort).then((collection)=>{
-        setChosenCollection(collection)})
-        .catch(e=>{console.log('setting error: ', e.message)});}
+    if (collectionId) {
+      getCollection(collectionId, sort)
+        .then((collection) => {
+          setChosenCollection(collection);
+        })
+        .catch((e) => {
+          console.log('setting error: ', e.message);
+        });
+    }
   }, [collectionId]);
 
   useEffect(()=>{
-    if(nftId){
-        getNft(nftId).then((nft)=>{
-        setChosenNft(nft)})
-        .catch(e=>{console.log('setting error: ', e.message)});}
+    if (nftId) {
+      getNft(nftId)
+        .then((nft) => {
+          setChosenNft(nft);
+        })
+        .catch((e) => {
+          console.log('setting error: ', e.message);
+        });
+    }
   }, [nftId]);
 
   useEffect(()=>{
-    getApp().then((app)=>{
-        setApp(app)})
-        .catch(e=>{console.log('setting error: ', e.message)});
+    getApp()
+      .then((app) => {
+        setApp(app);
+      })
+      .catch((e) => {
+        console.log('setting error: ', e.message);
+      });
   }, []);
   
+  if (!(chosenCollection && chosenSlot && chosenNft)) return emptyNode;
     
   return (
-    <> 
-      {chosenCollection && chosenSlot && chosenNft ? <>
-      <Box
-        sx={{
-          backgroundColor: 'none',
-          py: 5
-        }}
-      >
-      
-      <Box
-        sx={{
-          width: '85%',
-          alignSelf: 'stretch',
-          marginLeft: "auto",
-          marginRight: "auto",
-          py: 1,
-          px: 5,
-          backgroundColor: 'none'
-        }}
-      >
-      <Grid container spacing={2} minWidth="320px">
-      <Grid item xs={12}>
-              <Breadcrumbs aria-label="breadcrumb">
-                <NextLink underline="hover" color="inherit" href="/inventory">
-                  App
-                </NextLink>
-                <NextLink underline="hover" color="inherit" href={`/inventory/slot/${slotId}`}>
-                  Slot
-                </NextLink>
-                <NextLink underline="hover" color="inherit" href={`/inventory/slot/${slotId}/collection/${collectionId}`}>
-                  Collection
-                </NextLink>
-                <Typography color="text.primary">NFT</Typography>
-              </Breadcrumbs></Grid>
-        
-      <Grid item xs={12} sx={{backgroundColor: "none"}}>
-            <Typography variant="h3" sx={{ lineHeight:'40px'}}>
-            {chosenCollection.collectionName} #{chosenNft.serial}
-      </Typography>
-      <Typography variant="h5" sx={{lineHeight:{xs: '35px', lg:'80px'}}}>
-       Creator: {chosenCollection.handle} &emsp; App: {app.appName} &emsp; Slot: {chosenSlot.slotName} &emsp;
-      </Typography>
-      <Typography variant="p2" sx={{lineHeight:'25px', marginBottom:"40px"}}>
-       Total Supply: {chosenCollection.maximum} &emsp; Collection: {chosenCollection.collectionName} &emsp; Type: {chosenCollection.type} &emsp;
-      </Typography>
-      <Link href={"https://whatsonchain.com/tx/" + chosenNft.location.slice(0,-3)} variant="p2">
-  Location
-</Link>
+    <Box sx={{ backgroundColor: 'none', py: 5 }}>
+      <Box sx={{
+        width: '85%',
+        alignSelf: 'stretch',
+        marginLeft: "auto",
+        marginRight: "auto",
+        py: 1,
+        px: 5,
+        backgroundColor: 'none'
+      }}>
+        <Grid container spacing={2} minWidth="320px">
+          <Grid item xs={12}>
+            <Breadcrumbs aria-label="breadcrumb">
+              <NextLink underline="hover" color="inherit" href="/inventory">
+                App
+              </NextLink>
+              <NextLink underline="hover" color="inherit" href={`/inventory/slot/${slotId}`}>
+                Slot
+              </NextLink>
+              <NextLink underline="hover" color="inherit" href={`/inventory/slot/${slotId}/collection/${collectionId}`}>
+                Collection
+              </NextLink>
+              <Typography color="text.primary">
+                NFT
+              </Typography>
+            </Breadcrumbs>
+          </Grid>
+          <Grid item xs={12} sx={{ backgroundColor: "none" }}>
+            <Typography variant="h3" sx={{ lineHeight:'40px' }}>
+              {chosenCollection.collectionName} #{chosenNft.serial}
+            </Typography>
+            <Typography variant="h5" sx={{ lineHeight: { xs: '35px', lg:'80px' } }}>
+              Creator: {chosenCollection.handle} &emsp; App: {app.appName} &emsp; Slot: {chosenSlot.slotName} &emsp;
+            </Typography>
+            <Typography variant="p2" sx={{ lineHeight:'25px', marginBottom:"40px" }}>
+              Total Supply: {chosenCollection.maximum} &emsp; Collection: {chosenCollection.collectionName} &emsp; Type: {chosenCollection.type} &emsp;
+            </Typography>
+            <Link href={"https://whatsonchain.com/tx/" + chosenNft.location.slice(0,-3)} variant="p2">
+              Location
+            </Link>
             <NftDetailDisplay nft={chosenNft} setCurrentExpression={setCurrentExpression} currentExpression={currentExpression} />
+          </Grid>
+          <Grid item xs={12} sx={{ backgroundColor: "none" }}>
+            <NftPropertyDisplay nft={chosenNft} properties={properties} setProperties={setProperties} />
+          </Grid>
         </Grid>
-       <Grid item xs={12} sx={{backgroundColor: "none"}}>
-          <NftPropertyDisplay nft={chosenNft} properties={properties} setProperties={setProperties} />
-        </Grid></Grid>
-
-        </Box>
-  </Box> </>: <></>}
-        </>
+      </Box>
+    </Box>
   )
 }
 

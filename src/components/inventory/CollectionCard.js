b6e9d225452bf0c122b728460d314f3a085cf5df
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-04-04 19:12:54 -0700

    CollectionCard syntax 2

--- src/components/inventory/CollectionCard.js
+++ src/components/inventory/CollectionCard.js
@@ -7,13 +7,16 @@ var returnValue = <></>;
 export const CollectionCard = ({ search, collection, slot, collectionCount }) => {
   const searchTrue = collection.collectionName.toLowerCase().includes(search.toLowerCase());
 
-  if(collection.collectionImage){
-    if(collection.collectionImage.includes("http")){
-      collectionImage = collection.collectionImage
+  if (collection.collectionImage) {
+    if (collection.collectionImage.includes("http")) {
+      collectionImage = collection.collectionImage;
     }  
   }
 
   if (searchTrue) {
+    const fontSizeH4 = (collection.collectionName.length > 50) ? '10px' : { xs: '12px', sm: '14px', md: '14px', lg: '16px', xl: '14px' };
+    const fontSizeP2 = (collection.collectionName.length > 50) ? '10px' : { xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px' };
+
     returnValue = (
       <Grid item xs={12} sm={4} lg={3} onClick={()=>{}}>
         <NextLink href={`/inventory/slot/${slot.slotId}/collection/${collection.collectionId}`} passHref legacyBehavior>
@@ -24,67 +27,47 @@ export const CollectionCard = ({ search, collection, slot, collectionCount }) =>
             m: 1,
             minWidth: "320px"
           }}>
-            <img src={collectionImage} alt={'Collection Image'} style={{maxHeight: '300px', maxWidth: '300px', placeSelf: 'center'}} />
-            <Typography variant="h4" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '12px', sm: '14px', md: '14px', lg: '16px', xl: '14px'}}}>
+            <img src={collectionImage} alt="Collection Image" style={{ maxHeight: '300px', maxWidth: '300px', placeSelf: 'center' }} />
+            <Typography variant="h4" sx={{ lineHeight:"25px", fontSize: fontSizeH4 }}>
               { collection.collectionName }
             </Typography>
             <Box>
-              <Typography
-                variant="p2" sx={{fontWeight: "bold",lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ fontWeight: "bold", lineHeight:"25px", fontSize: fontSizeP2 }}>
                 Type:&nbsp;
               </Typography>
-              <Typography
-                variant="p2" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ lineHeight:"25px", fontSize: fontSizeP2 }}>
                 { collection.type }
               </Typography>
             </Box>
             <Box>
-              <Typography
-                variant="p2" sx={{fontWeight: "bold", lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ fontWeight: "bold", lineHeight:"25px", fontSize: fontSizeP2  }}>
                 Created by:&nbsp;
               </Typography>
-              <Typography
-                variant="p2" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ lineHeight:"25px", fontSize: fontSizeP2 }}>
                 { collection.handle }
               </Typography>
             </Box>
             <Box>
-              <Typography
-                variant="p2" sx={{fontWeight:"bold",lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ fontWeight:"bold", lineHeight:"25px", fontSize: fontSizeP2 }}>
                 Total Minted:&nbsp;
               </Typography>
-              <Typography
-                variant="p2" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ lineHeight:"25px", fontSize: fontSizeP2 }}>
                 { collection.minted }
               </Typography>
             </Box>
             <Box>
-              <Typography
-                variant="p2" sx={{fontWeight:"bold",lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ fontWeight:"bold", lineHeight:"25px", fontSize: fontSizeP2 }}>
                 Max Supply:&nbsp;
               </Typography>
-              <Typography
-                variant="p2" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
-                { collection.maximum>=900000000 ? '\u221e': collection.maximum }
+              <Typography variant="p2" sx={{ lineHeight:"25px", fontSize: fontSizeP2 }}>
+                { (collection.maximum >= 900000000) ? '\u221e': collection.maximum }
               </Typography>
             </Box>
             <Box>
-              <Typography
-                variant="p2" sx={{fontWeight:"bold",lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ fontWeight:"bold", lineHeight:"25px", fontSize: fontSizeP2 }}>
                 My Supply:&nbsp;
               </Typography>
-              <Typography
-                variant="p2" sx={{lineHeight:"25px", fontSize: collection.collectionName.length > 50?'10px':{ xs: '10px', sm: '10px', md: '12px', lg: '12px', xl: '14px'}}}
-              >
+              <Typography variant="p2" sx={{ lineHeight:"25px", fontSize: fontSizeP2 }}>
                 { collectionCount }
               </Typography>
             </Box>
@@ -93,6 +76,6 @@ export const CollectionCard = ({ search, collection, slot, collectionCount }) =>
       </Grid>
     );
   }
-  
+
   return returnValue;
 }

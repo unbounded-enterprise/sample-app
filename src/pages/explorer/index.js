e3fe2c08e78e5d50d0c834d6e1cb6c5309f32ffe
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-04-04 23:19:37 -0700

    explorer index syntax

--- src/pages/explorer/index.js
+++ src/pages/explorer/index.js
@@ -1,13 +1,10 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography, Grid } from '@mui/material';
 import { NewLayout } from '../../components/new-layout';
 import axios from 'axios';
-import React from 'react';
 import { SlotCard } from 'src/components/explorer/SlotCard';
 import { HomeHandcash } from 'src/components/home/home-handcash';
 import { useAuth } from 'src/hooks/use-auth';
-
-
 
 const ExplorerPage = () => {
   const [app, setApp] = useState(null);
@@ -16,100 +13,99 @@ const ExplorerPage = () => {
 
   const [chosenSlot, setChosenSlot] = useState(null);
 
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   useEffect(() => {
-    getSlots().then((slots) => {
-      setSlots(slots)
-    })
-      .catch(e => { console.log('setting error: ', e.message) });
+    getSlots()
+      .then((slots) => { 
+        setSlots(slots);
+      })
+      .catch(e => { console.log('setting error: ', e.message); });
   }, []);
 
 
   useEffect(() => {
-    sumCollections(slots).then((count) => {
-      setTotalCollections(count)
-    })
-      .catch(e => { console.log('setting error: ', e.message) });
+    sumCollections(slots)
+      .then((count) => {
+        setTotalCollections(count);
+      })
+      .catch((e) => { console.log('setting error: ', e.message); });
   }, [slots]);
 
   useEffect(() => {
-    getApp().then((app) => {
-      setApp(app)
-    })
-      .catch(e => { console.log('setting error: ', e.message) });
+    getApp()
+      .then((app) => {
+        setApp(app);
+      })
+      .catch((e) => { console.log('setting error: ', e.message); });
   }, []);
 
+  if (!user) return <HomeHandcash/>;
+
+  const fontSize = { xs: '12px', sm: '14px', md: '16px', lg: '16px', xl: '18px' };
+  const sharedSx = { font: 'nunito', lineHeight: '40px', fontSize };
+  const sharedSxBold = { fontWeight: 'bold', font: 'nunito', lineHeight: '40px', fontSize };
+
   return (
-    <>
-     {!user ? <>
-      <HomeHandcash></HomeHandcash> </> : <>
-      <Box
-        sx={{
-          backgroundColor: 'none',
-          py: 5
-        }}
-      >
-
-        <Box
-          sx={{
-            width: '85%',
-            alignSelf: 'stretch',
-            marginLeft: "auto",
-            marginRight: "auto",
-            py: 1,
-            px: 5,
-            backgroundColor: 'none'
-          }}
-        >
-          <Grid container spacing={2}>
-            <Grid item>
-              {app && !chosenSlot ? <>
-                <Typography variant="h2" sx={{ marginBottom: '5px' }}>
-                  NFT Explorer
-                </Typography>
-                <Typography variant="p2" sx={{ fontWeight: 'bold', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16px', xl: '18px' } }}>
-                  App:&nbsp;
-                </Typography>
-                <Typography variant="p2" sx={{ font: 'nunito', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16x', xl: '18px' } }}>
-                  {app.appName} <br></br>
-                </Typography>
-                <Typography variant="p2" sx={{ font: 'nunito', fontWeight: 'bold', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16x', xl: '18px' } }}>
-                  Total Slots:&nbsp;
-                </Typography>
-                <Typography variant="p2" sx={{ font: 'nunito', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16x', xl: '18px' } }}>
-                  {slots.length} &emsp;
-                </Typography>
-                <Typography variant="p2" sx={{ font: 'nunito', fontWeight: 'bold', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16x', xl: '18px' } }}>
-                  Total Collections:&nbsp;
-                </Typography>
-                <Typography variant="p2" sx={{ font: 'nunito', lineHeight: '40px', fontSize: { xs: '12px', sm: '14px', md: '16px', lg: '16x', xl: '18px' } }}>
-                  {totalCollections} <br></br>
-                </Typography>
-                <Typography variant="h4" sx={{ font: 'nunito', lineHeight: '50px' }}>
-                  Select Slot:
-                </Typography></>
-                : <></>}</Grid>
-            <Grid item>
-              <Grid container spacing={2}>
-                {slots && slots.map((slot) => (
-
-                  <React.Fragment key={slot.slotId}>
-                    <SlotCard slot={slot} setChosenSlot={setChosenSlot} />
-                  </React.Fragment>
-                ))}</Grid></Grid>
-
-
+    <Box sx={{ backgroundColor: 'none', py: 5 }}>
+      <Box sx={{
+        width: '85%',
+        alignSelf: 'stretch',
+        marginLeft: "auto",
+        marginRight: "auto",
+        py: 1,
+        px: 5,
+        backgroundColor: 'none'
+      }}>
+        <Grid container spacing={2}>
+          <Grid item>
+            { app && !chosenSlot && <>
+              <Typography variant="h2" sx={{ marginBottom: '5px' }}>
+                NFT Explorer
+              </Typography>
+              <Typography variant="p2" sx={{ fontWeight: 'bold', lineHeight: '40px', fontSize }}>
+                App:&nbsp;
+              </Typography>
+              <Typography variant="p2" sx={sharedSx}>
+                { app.appName }
+                <br></br>
+              </Typography>
+              <Typography variant="p2" sx={sharedSxBold}>
+                Total Slots:&nbsp;
+              </Typography>
+              <Typography variant="p2" sx={sharedSx}>
+                {slots.length} &emsp;
+              </Typography>
+              <Typography variant="p2" sx={sharedSxBold}>
+                Total Collections:&nbsp;
+              </Typography>
+              <Typography variant="p2" sx={sharedSx}>
+                { totalCollections }
+                <br></br>
+              </Typography>
+              <Typography variant="h4" sx={{ font: 'nunito', lineHeight: '50px' }}>
+                Select Slot:
+              </Typography>
+            </> }
           </Grid>
-        </Box>
+          <Grid item>
+            <Grid container spacing={2}>
+              { slots && slots.map((slot) => (
+                <React.Fragment key={slot.slotId}>
+                  <SlotCard slot={slot} setChosenSlot={setChosenSlot} />
+                </React.Fragment>
+              )) }
+            </Grid>
+          </Grid>
+        </Grid>
       </Box>
-    </>}</>
+    </Box>
   )
 }
 
 ExplorerPage.getLayout = (page) => (
   <NewLayout>
-    {page}
+    { page }
   </NewLayout>
 );
 
@@ -127,8 +123,8 @@ const getSlots = async () => {
 }
 
 const sumCollections = async (slots) => {
-  var collections = 0;
-  slots.forEach(element => {
+  let collections = 0;
+  slots.forEach((element) => {
     collections = collections + element.collections.length
   });
   return collections;

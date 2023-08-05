a51dd018a56999c49c469f7e644e7462f432c544
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-08-04 18:00:11 -0700

    iframe test

--- src/pages/play.js
+++ src/pages/play.js
@@ -1,7 +1,48 @@
-import React, { Fragment, useCallback, useEffect, useState } from "react";
+import React, { Fragment, useCallback, useEffect, useRef, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import { BasicTextField } from "../components/widgets/basic/basic-textfield";
+import { Dialog } from "@mui/material";
 
 const Play = () => {
+  const [email, setEmail] = useState('');
+  const runOncePleaseRef = useRef(false);
+
+  function handleClickMagicLogin() {
+    console.log('click!')
+  }
+  function handleEmailChange(e) {
+    console.log('e!')
+    console.log(e.target.value)
+  }
+
+  useEffect(() => {
+    if (runOncePleaseRef.current) return;
+
+    var iframe = document.createElement('iframe');
+    iframe.src = '/unity/index.html';
+    iframe.style.position = 'absolute';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    document.body.appendChild(iframe);
+
+    return () => {
+      runOncePleaseRef.current = true;
+    }
+  }, []);
+
+  return (<Fragment>
+    <input value={email} onChange={handleEmailChange}/>
+    <button onClick={handleClickMagicLogin}>
+        Login with Magic
+    </button>
+  </Fragment>);
+}
+
+const UnityIFrame = () => {
+  return <iframe src="/unity/index.html" style="width: 800px; height: 600px;"></iframe>
+}
+
+const PlayUnity = () => {
   const { unityProvider, loadingProgression, isLoaded, sendMessage, addEventListener, removeEventListener } = useUnityContext({
     loaderUrl: "unity/Build/magic-test-build.loader.js",
     dataUrl: "unity/Build/magic-test-build.data",
@@ -21,9 +62,6 @@ const Play = () => {
   function handleClickSpawnEnemies() {
     // sendMessage("GameController", "SpawnEnemies", 100);
   }
-  function handleClickMagicLogin() {
-    
-  }
 
   useEffect(() => {
     // addEventListener("GameOver", handleGameOver);
@@ -35,11 +73,8 @@ const Play = () => {
     { !isLoaded && (
       <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
     ) }
-    <button onClick={handleClickMagicLogin}>
-        Login with Magic
-    </button>
     <Unity unityProvider={unityProvider}
-      style={{ visibility: isLoaded ? "visible" : "hidden" }}
+      style={{ visibility: (isLoaded) ? "visible" : "hidden" }}
     />
   </Fragment>);
 }

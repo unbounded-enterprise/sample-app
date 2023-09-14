ffb76a1c099bb21456c7be6e108ab1540d6412f5
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-09-13 20:34:18 -0700

    expand stripe implementation

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -18,6 +18,7 @@ import {
   Stack,
   SvgIcon,
   MenuItem,
+  LinearProgress,
 } from "@mui/material";
 import { MainLayout } from "src/components/main-layout";
 import { CollectionCard } from "src/components/shop/CollectionCard";
@@ -41,11 +42,15 @@ import { BasicTextField } from "src/components/widgets/basic/basic-textfield";
 import { basicClone } from "src/utils/basic/basic-format.ts";
 import { loadStripe } from "@stripe/stripe-js";
 import {
+  AddressElement,
   CardElement,
   Elements,
+  LinkAuthenticationElement,
+  PaymentElement,
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
+import { StripeWordMark } from "src/icons/Stripe wordmark - blurple.js";
 
 export const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
@@ -122,6 +127,30 @@ const creditCardFieldSx = {
     border: "2px solid #045CD2",
   },
 };
+const stripeElementsOptions = {  
+  appearance: {
+    theme: 'stripe',
+    labels: 'floating',
+    /*
+    variables: {
+      colorPrimary: '#ffffff',
+      colorBackground: '#aaaaaa',
+      colorText: '#ffffff',
+      colorDanger: '#ffffff',
+      fontFamily: 'Ideal Sans, system-ui, sans-serif',
+      spacingUnit: '2px',
+      borderRadius: '4px',
+      // See all possible variables below
+    }
+    */
+    rules: {
+      '.Label': { color: '#929394' },
+      '.Input': { borderRadius: '8px' },
+      '.Input::placeholder': { color: '#929394' },
+    }
+  }
+};
+const cardElementOptions = { style: { base: { '::placeholder': { color: '#929394' } } } };
 
 const loading = (
   <>
@@ -455,6 +484,68 @@ const PurchaseCoinsCurrencyButtons = ({ setCurrency }) => {
   );
 };
 
+const StripeCheckoutForm = ({ user }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      redirect: 'if_required',
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        // return_url: "http://localhost:3000",
+      },
+    });
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error) {
+      if (error.type === "card_error" || error.type === "validation_error") {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("An unexpected error occurred.");
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <Stack sx={{ maxWidth: { md: "50%" } }}>
+      <PaymentElement options={{ defaultValues: { billingDetails: { email: user.email } } }}/>
+      <Box sx={{ height: '0.8rem' }}/>
+      <AddressElement options={{ mode: 'billing' }}/>
+      <Typography variant="subtitle2" sx={{ my: '1rem', fontSize: '14px' }}>
+        { `You'll have a chance to review your order before it's placed.` }
+      </Typography>
+      { (isLoading) ? (
+        <LinearProgress sx={{ my: '1rem', width: '100%' }}/>
+      ) : (<>
+        { errorMessage && <Typography sx={{ color: 'red' }}>{ errorMessage }</Typography> }
+        <Button onClick={handleSubmit} disabled={isLoading} sx={{ color: 'white', backgroundColor: '#045CD2' }}>
+          Next
+        </Button>
+      </>) }
+    </Stack>
+  );
+};
+
 const ShopContent = ({ user, balance, collections }) => {
   const [selectedBundle, setSelectedBundle] = useState(undefined);
   const [selectedCurrency, setSelectedCurrency] = useState("");
@@ -549,46 +640,57 @@ const ShopContent = ({ user, balance, collections }) => {
             spacing="1rem"
             sx={{ justifyContent: "space-between", width: "100%", px: "5rem" }}
           >
-            <Stack>
-              <Typography
-                variant="h6"
-                color="#284B9B"
-                fontFamily="Chango"
-                sx={defaultTextSx}
-              >
-                Purchase {selectedBundle.quantity} Coins for:
-              </Typography>
-              <Typography
-                variant="h5"
-                color="#FF4D0D"
-                fontFamily="Chango"
-                sx={defaultTextSx}
-              >
-                ${selectedBundle.price}
-              </Typography>
-            </Stack>
-            <Elements stripe={stripePromise} key={paymentIntent?.client_secret}>
-              <PaymentFormProvider>
-                <Stack sx={{ maxWidth: { md: "50%" } }}>
-                  <BasicTextField size="small" placeholder="Name on Card" />
-                  <Box sx={creditCardFieldSx}>
-                    <CardElement />
-                  </Box>
-                  <BasicTextField size="small" placeholder="Card Number" />
-                  <BasicTextField size="small" placeholder="Street Address" />
-                  <BasicTextField
-                    size="small"
-                    placeholder="Apt, Suite, Unit, Etc. (Optional)"
+            <Stack sx={{ justifyContent: 'space-between' }}>
+              <Stack>
+                <Typography
+                  variant="h6"
+                  color="#284B9B"
+                  fontFamily="Chango"
+                  sx={defaultTextSx}
+                >
+                  Purchase {selectedBundle.quantity} Coins for:
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="#FF4D0D"
+                  fontFamily="Chango"
+                  sx={defaultTextSx}
+                >
+                  ${selectedBundle.price}
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: { xs: '2.5rem', md: '4rem' }, width: '100%' }}>
+                  <img
+                    src="/static/coinImage.png"
+                    alt=""
+                    style={{
+                      width: "12rem",
+                      height: "12rem",
+                    }}
                   />
-                  <BasicTextField size="small" placeholder="" />
-                  <BasicTextField size="small" placeholder="City" />
-                  <Stack direction="row">
-                    <BasicTextField size="small" placeholder="State" />
-                    <BasicTextField size="small" placeholder="Zip Code" />
-                  </Stack>
+                </Box>
+              </Stack>
+              <Stack>
+                <Box sx={{ minHeight: { xs: '4rem', md: '12rem'} }}/>
+                <Typography sx={{ fontSize: '16px' }}>Powered by</Typography>
+                <Stack direction="row" spacing="1rem" sx={{ alignItems: 'center' }}>
+                  <SvgIcon
+                    component={StripeWordMark}
+                    viewBox="0 0 368 222.5"
+                    sx={{ width: 'auto', height: '2.25rem' }}
+                  />
+                  <Typography sx={{ color: '#6B7280', fontSize: '14px' }}>|</Typography>
+                  <Typography sx={{ color: '#6B7280', fontSize: '14px' }}>Terms</Typography>
+                  <Typography sx={{ color: '#6B7280', fontSize: '14px' }}>Privacy</Typography>
                 </Stack>
-              </PaymentFormProvider>
-            </Elements>
+              </Stack>
+            </Stack>
+            { paymentIntent && 
+              <Elements stripe={stripePromise} options={{ clientSecret: paymentIntent?.client_secret, ...stripeElementsOptions}}>
+                <PaymentFormProvider>
+                  <StripeCheckoutForm user={user}/>
+                </PaymentFormProvider>
+              </Elements>
+            }
           </Stack>
         </Stack>
       )}
